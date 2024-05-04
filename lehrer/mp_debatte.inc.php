5a0b92cd7cfe68fdcadf1ec9bf5cd1bb472f6c4c
Christoph Kasseckert <c.kasseckert@me.com>
2024-05-04 13:31:04 +0200

    Debatte

--- lehrer/mp_debatte.inc.php
+++ lehrer/mp_debatte.inc.php
@@ -1,72 +1,85 @@
 <?php
-// Verbalgutachten - Projektart DEBATTE
-//
-// i = Sachkenntnis
-$i = 'Sachkenntnis';
-// 0 Punkte
-$i0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
-// 1 Punkt
-$i1a = 'kennt sich kaum aus';
-$i1b = 'vieles inhaltlich falsch';
-$i1c = 'manches nur zum Teil richtig';
-// 3 Punkte
-$i3a = 'kennt sich aus';
-$i3b = 'erfasst den Streitpunkt';
-$i3c = 'bleibt beim Thema, weiß einigermaßen Bescheid';
-// 5 Punkte
-$i5a = 'genau informiert';
-$i5b = 'gut vorbereitet';
-$i5c = 'Detailwissen und Kenntnis der Zusammenhänge';
-//
-// c = Ausdrucksvermögen
-$c = 'Ausdrucksvermögen';
-// 0 Punkte
-$c0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
-// 1 Punkt
-$c1a = 'gerade noch verständlich';
-$c1b = 'einigermaßen verständlich';
-$c1c = 'wenig Regung';
-// 3 Punkte
-$c3a = 'verständlich';
-$c3b = 'flüssig';
-$c3c = 'lebendig';
-// 5 Punkte
-$c5a = 'jederzeit angemessen';
-$c5b = 'einprägsam';
-$c5c = 'originell';
-//
-// p = Gesprächsfähigkeit
-$p = 'Gesprächsfähigkeit';
-// 0 Punkte
-$p0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
-// 1 Punkt
-$p1a = 'hört wenig zu';
-$p1b = 'redet kaum zu den Gesprächspartnern';
-$p1c = 'geht manchmal auf die anderen ein';
-// 3 Punkte
-$p3a = 'hört zum Teil gut zu';
-$p3b = 'berücksichtigt das Gesagte der anderen';
-$p3c = 'fasst gelegentlich den Stand der Debatte zusammen';
-// 5 Punkte
-$p5a = 'hört sehr aufmerksam zu';
-$p5b = 'führt Gedanken der anderen weiter';
-$p5c = 'hat immer alles im Blick';
-//
-// g = Überzeugungskraft
-$g = 'Überzeugungskraft';
-// 0 Punkte
-$g0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
-// 1 Punkt
-$g1a = 'nur Behauptungen ohne Begründungen';
-$g1b = 'kaum Hörerbezug';
-$g1c = 'eigene Rolle wenig glaubwürdig';
-// 3 Punkte
-$g3a = 'die meisten Gründe eher schwach';
-$g3b = 'teils schwache, teils gute Gründe';
-$g3c = 'nennt überhaupt Gründe';
-// 5 Punkte
-$g5a = 'gut begründet';
-$g5b = 'gut gewichtet';
-$g5c = 'insgesamt überzeugend';
-//
+    // Verbalgutachten - Projektart DEBATTE
+    // Quelle: https://www.lehrplanplus.bayern.de/sixcms/media.php/71/argumentieren_und_debattieren.pdf
+    //
+    // i = Inhalt
+    // ia = Informationsstand
+    // ib = fachliche Richtigkeit
+    // ic = Überblick
+    $i = 'Sachkenntnis';
+    // 0 Punkte
+    $i0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
+    // 1 Punkt
+    $i1a = 'Der eigene Informationsstand bleibt sehr lückenhaft.';
+    $i1b = 'Viele vorgebrachte Aspekte sind leider falsch.';
+    $i1c = 'Die gefragte Maßnahme wird zumindest grob umrissen.';
+    // 3 Punkte
+    $i3a = 'Der eigene Informationsstand ist weitgehend ausreichend.';
+    $i3b = 'Viele vorgebrachte Aspekte sind zum Teil richtig.';
+    $i3c = 'Die vorgebrachten Aspekte überzeugen durch umfassendes Detailwissen.';
+    // 5 Punkte
+    $i5a = 'Der eigene Informationsstand ist überzeugend.';
+    $i5b = 'Die gefragte Maßnahme wird genau bestimmt.';
+    $i5c = 'Die Kenntnis der Zusammenhänge ist beeindruckend.';
+    //
+    // c = Überzeugungskraft
+    // ca = Aufstellung von Thesen
+    // cb = Begründungen
+    // cc = eigener Standpunkt
+    $c = 'Ausdrucksvermögen';
+    // 0 Punkte
+    $c0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
+    // 1 Punkt
+    $c1a = 'Es werden nur Behauptungen angeführt.';
+    $c1b = 'Die genannten Gründe bleiben meistens eher schwach.';
+    $c1c = 'Die eigene Rolle bleibt wenig glaubwürdig ausgefüllt.';
+    // 3 Punkte
+    $c3a = 'Es wird unterschiedlich überzeugend begründet.';
+    $c3b = 'Die angeführten Gründe sind vielfach ansprechend.';
+    $c3c = 'Der eigene Standpunkt wird weitgehend sicher vertreten.';
+    // 5 Punkte
+    $c5a = 'Es wird in jeder Hinsicht überzeugend begründet.';
+    $c5b = 'Die angeführten Gründe sind glaubwürdig und nachvollziehbar.';
+    $c5c = 'Der Auftritt erfreut durch eine überzeugende Darlegung des eigenen Standpunkts im Fortschritt der Debatte.';
+    //
+    // p = Gesprächsfähigkeit
+    // pa = Zuhören
+    // pb = Eingehen auf die Anderen
+    // pc = Gesprächsbeteiligung
+    $p = 'Gesprächsfähigkeit';
+    // 0 Punkte
+    $p0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
+    // 1 Punkt
+    $p1a = 'Dem Gegenüber wird nur wenig zugehört.';
+    $p1b = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
+    $p1c = 'Es wird kaum mit den Gesprächspartnern geredet.';
+    // 3 Punkte
+    $p3a = 'Dem Gegenüber wird meist zugehört.';
+    $p3b = 'Die Inhalte der Anderen finden meist Berücksichtigung.';
+    $p3c = 'Gelegentlich werden Aspekte der Debatte zusammengefasst.';
+    // 5 Punkte
+    $p5a = 'Dem Gegenüber wird aufmerksam zugehört.';
+    $p5b = 'Die Gedanken der Anderen werden sinnvoll weitergeführt.';
+    $p5c = 'Zu jeder Zeit werden alle Argument und Inhalte im Blick gehalten.';
+    //
+    // g = Ausdrucksvermögen
+    // ga = Ausdrucksweise
+    // gb = sprachliche Gestaltung
+    // gc = Darstellungsweise
+    $g = 'Ausdrucksvermögen';
+    // 0 Punkte
+    $g0 = 'Das Ergebnis entspricht leider nicht den Mindestkriterien.';
+    // 1 Punkt
+    $g1a = 'Die Ausdrucksweise ist gerade noch verständlich.';
+    $g1b = 'Die sprachliche Gestaltung bleibt stockend und schwer nachvollziehbar.';
+    $g1c = 'Die Darstellungsweise bleibt unverständlich.';
+    // 3 Punkte
+    $g3a = 'Die Ausdrucksweise ist verständlich.';
+    $g3b = 'Die sprachliche Gestaltung ist flüssig und lebendig.';
+    $g3c = 'Die Darstellungsweise ist anschaulich.';
+    // 5 Punkte
+    $g5a = 'Die Ausdrucksweise ist sehr gut verständlich.';
+    $g5b = 'Die sprachliche Gestaltung überzeugt durch Klarheit und eine deutliche Gliederung.';
+    $g5c = 'Die Darstellungsweise ist einprägsam und originell.';
+    //
 ?>
